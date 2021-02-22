b9f4f3924c9e9f40c48bc19f6df809dab8224ac9
alextremmel <59810749+alextremmel@users.noreply.github.com>
2021-02-21 21:01:17 -0800

    Add files via upload

--- visual.py
+++ visual.py
@@ -4,7 +4,7 @@ from math import sqrt
 
 
 # base receiver
-base_receiver = [200, 150]
+base_receiver = [150, 125]
 # the [x, y] to be added to the the receiver
 # x distance on the x axis between each receiver, same for y
 receiver_adder = [150, 75]
@@ -23,12 +23,12 @@ def plt_circle(x, y, radius):
 
 def plt_point(x, y, c):
 	if c == 'r+':
-		plt.plot(x, y, 'r+')
+		plt.plot(x, y, 'r+', markersize=8)
 	elif c == 'b+':
-		plt.plot(x, y, 'b+')
+		plt.plot(x, y, 'b+', markersize=8)
 
-def plt_text(x, y, text):
-	ax.text(x, y, text)
+def plt_text(x, y, text, size):
+	ax.text(x, y, text, size=size)
 
 
 # creates a list of [x, y] for each receiver using the base and the adder
@@ -96,23 +96,51 @@ def average_position(senders):
 def text_receiver(receivers, senders):
 	average_pos = average_position(senders)
 	for i in receivers:
-		plt_text(i[0], i[1], round(point_distance(i[0], i[1], average_pos[0], average_pos[1]), 1))
+		plt_text(i[0], i[1], round(point_distance(i[0], i[1], average_pos[0], average_pos[1]), 1), 10)
 
 
+# function for the legend or the description
 
+def description():
+	ax.add_patch(patches.Rectangle((1200, 50), 350, 200, color="w", zorder=2, ec="r"))
+	plt_point(1220, 220, 'r+')
+	plt_text(1220, 220, "  distance", 10)
+	plt_text(1205, 160, "red + is a space ship, distance is the distance from\n the space ship to the average satellite position", 12)
+	plt_point(1220, 120, 'b+')
+	plt_text(1210, 70, "blue + is a satellite", 12)
+	circle = plt.Circle((1375, 120), 15, fill = False, zorder=3)
+	ax.add_artist(circle)
+	plt_text(1400, 60, "there are 3 circles\n intersecting at each\n ship each with\n a center at a\n different satellite", 12)
+
+# function to explain the goal of the visual
+
+def goal():
+	ax.add_patch(patches.Rectangle((50, 680), 350, 170, color="w", zorder=2, ec="r"))
+	text = '''
+This visual shows that the further the space ship
+gets from the satellites, the lower the location
+accuracy is, or if the red + was not there, the
+harder it would be to see its exact position.
+As the visual shows, with the closest space ship,
+even without the red +, it is easy to tell where the
+circles intersect, but as the distance increases,
+the red + is more and more needed
+to show exactly were the circles intersect.'''
+	plt_text(55, 690, text, 12)
 
 
 
 fig, ax = plt.subplots()
 ax.set(xlim=(0, 1600), ylim = (0, 900))
-
-
+plt.title("Accuracy Fall-off Over Distance")
+plt.xlabel("Arbitrary Unit")
+plt.ylabel("Arbitrary Unit")
 
 receivers = receiver_points(base_receiver, receiver_adder, receiver_num)
 plt_receiver(receivers)
 plt_sender(senders)
 plt_signal(receivers, senders)
 text_receiver(receivers, senders)
-
-
+description()
+goal()
 plt.show()
